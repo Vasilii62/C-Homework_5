a288af0bce80ace53c991da50a2e90eeb0f5ef8c
email <vassili642@gmail.com>
2022-11-17 20:07:31 +0300

    add

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 //Напишите программу, которая покажет количество чётных чисел в массиве.
 
 
-Console.WriteLine("Enter the size of the array: ");
+/*Console.WriteLine("Enter the size of the array: ");
 int size = Convert.ToInt32(Console.ReadLine());
 int min = 100;
 int max = 999;
@@ -31,5 +31,21 @@ void FindEven(int[] MyArray)
         }
     }
     Console.WriteLine($"Even numbers {even}");
-}
+}*/
 
+// Задача 36:Задайте массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+int result = 0;
+ Console.WriteLine("Enter the number of array elements: ");
+ int size = Convert.ToInt32(Console.ReadLine());
+ int[] array = new int[size];
+ for(int i = 0; i < array.Length; i++)
+ {
+    array[i] = Convert.ToInt32(Console.ReadLine());
+ }
+ if (array[i] %2 != 0)
+ {
+    result += array[i];
+ }
+ Console.WriteLine($"The sum of odd array elements is {result}");
